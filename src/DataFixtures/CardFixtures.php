24cfe39b8ddee8daf2c598d369798c61d17d9fa2
marcinpank <marcin.pankiewicz@gmail.com>
2021-04-01 10:36:30 +0200

    Add card fixtures

---
+++ src/DataFixtures/CardFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Card;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CardFixtures extends Fixture
+{
+    const CARDS =  [
+        'Geralt' => 10,
+        'Ciri' => 9,
+        'Vesemir' => 5,
+        'Triss' => 3,
+        'Aard sign' => 0
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CARDS as $title => $power) {
+            $card = new Card();
+            $card->setTitle($title);
+            $card->setPower($power);
+            $manager->persist($card);
+        }
+        $manager->flush();
+    }
+}
